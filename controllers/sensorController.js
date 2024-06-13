3256d61dbd4d2640583b05dbf774c3a5e1616121
amier <roslanamier27@gmail.com>
2024-06-14 01:42:51 +0800

    get fall data

--- controllers/sensorController.js
+++ controllers/sensorController.js
@@ -16,13 +16,12 @@ exports.getSensorData = (req, res) => {
 exports.storeSensorData = async (req, res) => {
   const { date, fall } = req.body;
 
-  if (date && fall !== undefined) {
+  if (date !== undefined && fall !== undefined) {
     try {
       const db = admin.database();
       const ref = db.ref('falls');
       await ref.push({ date, fall });
 
-      latestSensorData = { date, fall };  // Update latest data
       console.log('Data stored to Firebase:', { date, fall });
 
       res.status(200).send('Data sent to Firebase successfully');
@@ -34,3 +33,26 @@ exports.storeSensorData = async (req, res) => {
     res.status(400).send('Invalid data');
   }
 };
+
+exports.getHistoricalSensorData = async (req, res) => {
+  try {
+    const db = admin.database();
+    const ref = db.ref('falls');
+    const snapshot = await ref.once('value');
+
+    const data = snapshot.val();
+    const historicalData = Object.keys(data).map(key => {
+      return {
+        date: data[key].date,
+        fall: data[key].fall
+      };
+    });
+    console.error('Success fetching historical sensor data:', historicalData);
+
+    res.status(200).json(historicalData);
+  } catch (error) {
+    console.error('Error fetching historical sensor data:', error);
+    res.status(500).send('Error fetching historical sensor data');
+  }
+};
+
--- routes/sensorRoutes.js
+++ routes/sensorRoutes.js
@@ -5,5 +5,6 @@ const sensorController = require('../controllers/sensorController');
 router.post('/', sensorController.postSensorData);
 router.get('/', sensorController.getSensorData);
 router.post('/store', sensorController.storeSensorData);
+router.get('/firebase', sensorController.getHistoricalSensorData); // New route for fetching historical data
 
 module.exports = router;
